e9e48d149bf544190eaedb452f781f22b22da968
Seth Carter <carter.seth.m@gmail.com>
2020-04-27 09:10:59 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    title: `The Localhost Blog`,
+    description: `This is a test blog for practice purposes`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `posts`,
+      },
+    },
+  ],
+};
